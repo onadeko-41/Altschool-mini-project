658b2083ea41b2a2ef1949a4f3848ddf2b98537a
onadeko-41 <104556847+onadeko-41@users.noreply.github.com>
2023-03-19 20:23:06 +0100

    Add files via upload

---
+++ Exam Folder/eks.tf
@@ -0,0 +1,81 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "my-eks"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key        = "onadeko"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+    spot2 = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot2"
+      }
+
+      taints = [{
+        key       = "onadeko"
+        value  = "spot2"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = {
+    Environment = "staging"
+  }
+}
+data "aws_eks_cluster" "cluster" {
+    name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+    name = module.eks.cluster_id
+}
